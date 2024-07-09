fcd81f3f0385dca93b6f4085cd7876110b36573a
Render <27890173+itsrenderman@users.noreply.github.com>
2024-07-09 07:55:09 +0100

    Add files via upload

---
+++ Table.lua
@@ -0,0 +1,37 @@
+--!strict
+
+local Table = {
+	clear = table.clear,
+	clone = table.clone,
+	concat = table.concat,
+	create = table.create,
+	find = table.find,
+	foreach = function(t: {[any]: any}, f: (any, any) -> ()): ()
+		for k, v in t do
+			f(k, v)
+		end
+	end,
+	foreachi = function(t: {[any]: any}, f: (any, any) -> ()): ()
+		for i, v in ipairs(t) do
+			f(i, v)
+		end
+	end,
+	freeze = table.freeze,
+	getn = function(t: {[any]: any}): number
+		local i: number = 0
+		for _ in t do
+			i += 1
+		end
+		return i
+	end,
+	insert = table.insert,
+	isfrozen = table.isfrozen,
+	maxn = table.maxn,
+	move = table.move,
+	pack = table.pack,
+	remove = table.remove,
+	sort = table.sort,
+	unpack = table.unpack
+}
+
+return Table
