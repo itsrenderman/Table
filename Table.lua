4af6b881627105b3899bd0d4e5e0002a6f314534
Render <27890173+itsrenderman@users.noreply.github.com>
2025-05-26 06:32:10 +0100

    refactor: add [isarray, filter], improve [remove]

--- Table.lua
+++ Table.lua
@@ -3,6 +3,18 @@
 local Table = {}
 
 Table.clear = table.clear
+Table.concat = table.concat
+Table.create = table.create
+Table.find = table.find
+Table.freeze = table.freeze
+Table.insert = table.insert
+Table.isfrozen = table.isfrozen
+Table.maxn = table.maxn
+Table.move = table.move
+Table.pack = table.pack
+Table.sort = table.sort
+Table.unpack = table.unpack
+
 Table.clone = function(tbl: {[any]: any}, shouldDeepClone: boolean?): {[any]: any}
 	if shouldDeepClone then
 		local cloneTbl = Table.clone(tbl, false)
@@ -15,9 +27,6 @@ Table.clone = function(tbl: {[any]: any}, shouldDeepClone: boolean?): {[any]: an
 	end
 	return table.clone(tbl)
 end
-Table.concat = table.concat
-Table.create = table.create
-Table.find = table.find
 Table.foreach = function(tbl: {[any]: any}, f: (any, any, {[any]: any}) -> ()): ()
 	for k, v in tbl do
 		f(v, k, tbl)
@@ -28,7 +37,6 @@ Table.foreachi = function(tbl: {[any]: any}, f: (any, number, {[any]: any}) -> (
 		f(v, i, tbl)
 	end
 end
-Table.freeze = table.freeze
 Table.getn = function(tbl: {[any]: any}): number
 	local i: number = 0
 	for _ in tbl do
@@ -36,13 +44,36 @@ Table.getn = function(tbl: {[any]: any}): number
 	end
 	return i
 end
-Table.insert = table.insert
-Table.isfrozen = table.isfrozen
-Table.maxn = table.maxn
-Table.move = table.move
-Table.pack = table.pack
-Table.remove = table.remove
-Table.sort = table.sort
-Table.unpack = table.unpack
+Table.isarray = function(tbl: {[any]: any}): boolean
+	return #tbl == Table.getn(tbl)
+end
+Table.remove = function(tbl: {[any]: any}, k: any): any?
+	local v: any? = nil
+	if Table.isarray(tbl) then
+		v = table.remove(tbl, k)
+	else
+		v = tbl[k]
+		tbl[k] = nil
+	end
+	return v
+end
+Table.filter = function(tbl: {[any]: any}, f: (any, any, {[any]: any}) -> boolean): {[any]: any}
+	local isArray: boolean = Table.isarray(tbl)
+	tbl = Table.clone(tbl, true)
+
+	local hasRemovedSomething: boolean = false
+
+	repeat
+		hasRemovedSomething = false
+		Table.foreach(tbl, function(v, k)
+			if f(v, k, tbl) then
+				return
+			end
+			hasRemovedSomething = true
+			Table.remove(tbl, k)
+		end)
+	until not hasRemovedSomething
+	return tbl
+end
 
 return Table
